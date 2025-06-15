dd37dd9c350b925ed284a37888914aae2f8a8d0a
dov zucker <ydovzucker@gmail.com>
2025-06-16 00:11:17 +0300

    crated abstract class for agents

---
+++ IranianAgent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Security;
+
+abstract class IranianAgent
+{
+    public readonly string name;
+    public readonly int id;
+    private readonly Sensor[] agentVulnerabilityList;
+    private readonly Sensor[] attachedSensors;
+
+
+    public IranianAgent(string name,int id)
+    {
+        this.name = name;
+        this.id = id;
+        
+
+    }
+
+}
